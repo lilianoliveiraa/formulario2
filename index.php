31fd44bb05673a807deb9e1a8cff6ccd95b23bb3
lilianoliveiraa <ad_lilian@yahoo.com.br>
2019-10-07 15:17:15 -0300

    form test

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <link rel="stylesheet" type="text/css" href="/base.css" >
 
 <form action="/script.php" method="post">
